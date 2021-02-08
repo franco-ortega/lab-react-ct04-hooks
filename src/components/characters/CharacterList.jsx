fa4294e1a0309b7af174aee97d3406eaf48630a0
Franco Ortega <francopdx@gmail.com>
2021-02-08 15:36:02 -0800

    add useState and useEffect to CharacterList; update/correct getCharacters()

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getCharacters } from '../services/getCharacters';
 
 export default function CharacterList() {
-    return (
-        <div>
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters()
+      .then((newCharacters) => setCharacters(newCharacters));
+  }, []);
+
+  console.log(characters);
+
+
+  return (
+    <div>
             Character List
-        </div>
-    )
+    </div>
+  );
 }
--- src/components/services/getCharacters.jsx
+++ src/components/services/getCharacters.jsx
@@ -1,6 +1,6 @@
-export const getCharacters = () => {
-  return fetch('https://ac-vill.herokuapp.com/villagers')
-    .then((res) => res.json)
+export const getCharacters = async() => {
+  return await fetch('https://ac-vill.herokuapp.com/villagers')
+    .then((res) => res.json())
     .then((characters) => characters.map(character => ({
       name: character.name,
       japaneseName: character.japaneseName,
@@ -10,8 +10,8 @@ export const getCharacters = () => {
       species: character.species,
       skill: character.skill,
       goal: character.goal,
-      roast: characters.coffee.roast,
-      milk: characters.coffee.milk,
-      sugar: characters.coffee.sugar
+      roast: character.coffee.roast,
+      milk: character.coffee.milk,
+      sugar: character.coffee.sugar
     })));
 };
