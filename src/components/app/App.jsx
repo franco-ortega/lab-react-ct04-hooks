63ebc229220354dbd0556ca3011f6eb398d88754
Franco Ortega <francopdx@gmail.com>
2021-02-08 14:52:41 -0800

    add router, switch, and routes to App; add initial framework to CharacterList, Character, and CharacterDetails

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import CharacterList from '../characters/CharacterList';
+import CharacterDetails from '../characters/CharacterDetails';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <h1>Animal Crossing Characters</h1>
+      <Switch>
+        <Route exact path="/" component={CharacterList} />
+        <Route exact path="/character/:id" component={CharacterDetails} />
+      </Switch>
+    </ Router>
+  );
 }
